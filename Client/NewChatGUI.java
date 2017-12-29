947472f47e85b22143a956f64b6f3275da1f14b6
Taslima-Begum <34741987+Taslima-Begum@users.noreply.github.com>
2017-12-29 07:44:35 +0000

    Revert "Merge branch 'master' of https://github.com/Taslima-Begum/CN5122-"
    This reverts commit 9675162f591403074a9d35f08cc79a72b7966743, reversing
    changes made to 77790775240dd4dd4fb6d908eb61d1063eacb6ca.

--- Client/NewChatGUI.java
+++ Client/NewChatGUI.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import java.awt.*;
 import javax.swing.*;
@@ -67,74 +66,4 @@ public class NewChatGUI extends JFrame implements ActionListener {
 			
 		}
 	}
-=======
-
-import java.awt.*;
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import java.awt.event.*;
-import java.util.*;
-import java.awt.*;
-
-public class NewChatGUI extends JFrame implements ActionListener {
-
-	private JPanel contentPane = new JPanel();
-	JTextField chatNameField = new JTextField();
-	JLabel lblNewLabel = new JLabel("Start a new Chat");
-	JButton nextBtn = new JButton("Next");
-	JLabel lblEnterChatName = new JLabel("Chat Name:");
-	JLabel errorLabel = new JLabel("");
-	chatGUI a;
-	
-	public NewChatGUI() {
-		setBounds(100, 100, 552, 438);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-	
-		contentPane.setLayout(null);
-		lblNewLabel.setBounds(99, 9, 314, 45);
-		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD, 15));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		contentPane.add(lblNewLabel);
-	
-		nextBtn.addActionListener(this);
-		nextBtn.setBounds(405, 335, 95, 29);
-		contentPane.add(nextBtn);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(32, 80, 468, 208);
-		
-		panel.setLayout(new BorderLayout(0, 0));
-		contentPane.add(panel);
-		chatNameField.setBounds(151, 339, 209, 20);
-		contentPane.add(chatNameField);
-		chatNameField.setColumns(10);
-		lblEnterChatName.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		lblEnterChatName.setBounds(60, 339, 81, 20);
-		contentPane.add(lblEnterChatName);
-		errorLabel.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		errorLabel.setForeground(Color.RED);
-		
-
-		errorLabel.setBounds(151, 367, 209, 14);
-		contentPane.add(errorLabel);
-		setContentPane(contentPane);
-		setLocationRelativeTo(null);
-		setVisible(true);
-	}
-
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==nextBtn) {
-			if(chatNameField.getText().equals("")) {
-				errorLabel.setText("Error! Please enter Chat name");
-			}
-			else {
-				a =new chatGUI(chatNameField.getText());
-				chats.addchat(a);
-				setVisible(false);
-			}
-			
-		}
-	}
->>>>>>> a0fa085f58d915215188a0bb98f0e2bb8742b9f0
 }
--- Client/chats.java
+++ Client/chats.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import java.awt.BorderLayout;
 import java.util.ArrayList;
@@ -20,27 +19,4 @@ public class chats {
 		chat.messages.append(srcUser + ": " + message);
 	}
 	
-=======
-
-import java.awt.BorderLayout;
-import java.util.ArrayList;
-
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JSeparator;
-
-public class chats {
-	static ArrayList<chatGUI>listChats= new ArrayList<>();
-	
-	public static void addchat(chatGUI c) {
-		listChats.add(c);
-		MainActivityGUI.d.addElement(c);
-		System.out.print(listChats.size());
-	}
-	
-	public static synchronized void updatechat(chatGUI chat, String srcUser, String message) {
-		chat.messages.append(srcUser + ": " + message);
-	}
-	
->>>>>>> a0fa085f58d915215188a0bb98f0e2bb8742b9f0
 }
