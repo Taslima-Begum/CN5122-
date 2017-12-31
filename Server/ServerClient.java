24c06d287008d693e9b5c12606eec98820fad7e7
Taslima-Begum <34741987+Taslima-Begum@users.noreply.github.com>
2017-12-31 01:28:35 +0000

    updated

---
+++ Server/ServerClient.java
@@ -0,0 +1,39 @@
+public abstract class ServerClient {
+
+    private final String key = "cn5120!?";  // key for encryption/decryption
+    private final String host = "localhost";
+    private final int portNumber = 5000;
+    private Algorithm alg = new Algorithm();
+
+    /**
+     * Encrypt a message using homemade ECB
+     *
+     * @param m the message to be encrypted
+     * @return encrypted String
+     */
+    protected String encryptMessage(String m) {
+        int[] temp = alg.ECB(m, key, false);
+        return alg.convertToString(temp);
+    }
+
+    /**
+     * Decrypt a message using homemade ECB
+     *
+     * @param m the message to be decrypted
+     * @return decrypted String
+     */
+    protected String decryptMessage(String m) {
+        int[] temp = alg.ECB(m, key, true);
+        return alg.convertToString(temp);
+    }
+
+    // getter methods here..
+    protected int getPortNumber() {
+        return portNumber;
+    }
+
+    protected String getHost() {
+        return host;
+    }
+
+}
